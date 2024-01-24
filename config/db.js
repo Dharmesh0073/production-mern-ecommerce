a3f7951784637df07305550722dae1e22ff59925
Dharmesh0073 <105600746+Dharmesh0073@users.noreply.github.com>
2024-01-24 20:56:14 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async()=>{
     try{
-       const conn = await mongoose.connect(mongodb+srv://dharmesh:dharmesh1@ecommerce.pedkfcs.mongodb.net/ecommerce)
+       const conn = await mongoose.connect('mongodb+srv://dharmesh:dharmesh1@ecommerce.pedkfcs.mongodb.net/ecommerce')
        console.log(`connected to mongodb daabase${conn.connection.host}`)
     }catch(error){
                console.log(`Error in mongodb${error}`)
